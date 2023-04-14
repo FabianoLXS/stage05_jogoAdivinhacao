bc6438e9dc7d83e1af415bb0c1e0df3f78fae8e5
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-04-14 09:05:28 -0300

    commit

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-//variaveis
-const randomNumber = Math.round(Math.random() * 10)
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-
 // Eventos
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
+})
+
+// funções
 function handleTryClick(event) {
-  event.preventDefault()
+  event.preventDefault() // nao faca o padrao
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `acertou em ${xAttempts} tentativas`
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
   }
+
   inputNumber.value = ""
   xAttempts++
 }
-//Função callBack
-function handleResetClick(event) {
-  event.preventDefault()
 
-    document.querySelector(".screen1").classList.remove("hide")
-    document.querySelector(".screen2").classList.add("hide")
-    xAttempts = 1
-  }
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+}
 
-
-
-btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", handleResetClick)
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
